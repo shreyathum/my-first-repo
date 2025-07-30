4a7fc5360ede269de0d1434b0c07140178766592
shreyathum <srt348@stern.nyu.edu>
2025-07-29 20:23:30 -0400

    able to run tests, fixed the bug

--- app/rps.py
+++ app/rps.py
@@ -1,17 +1,8 @@
 # this is my rock paper scissors game 
 
-print("Welcome to my game")
-
-player_choice = input("Please select an option ('rock', 'paper', 'scissors'): ")
-print("USER CHOSE:", player_choice)
-
-#todo: validate choice 
-
 import random
 
 valid_options = ["rock", "paper", "scissors"]
-computer_choice = random.choice(valid_options)
-print("COMPUTER CHOSE:", computer_choice)
 
 def determine_winner(u, c): 
     if u == c:
@@ -27,8 +18,22 @@ def determine_winner(u, c):
     elif u == "paper" and c == "scissors": 
         result = "COMP WINS"
     elif u == "paper" and c == "rock": 
-        result = "COMP WINS"
+        #result = "COMP WINS" #oops bug , that was a bug 
+        result = "USER WINS"
     return result 
 
-result_message = determine_winner(player_choice, computer_choice)
-print(result_message)
+#only run the code indented inside if we are running the script from the command line 
+#but NOT if we are importing 
+if __name__ == "__main__": 
+
+    print("Welcome to my game")
+
+    player_choice = input("Please select an option ('rock', 'paper', 'scissors'): ")
+    print("USER CHOSE:", player_choice)
+
+
+    computer_choice = random.choice(valid_options)
+    print("COMPUTER CHOSE:", computer_choice)
+
+    result_message = determine_winner(player_choice, computer_choice)
+    print(result_message)
